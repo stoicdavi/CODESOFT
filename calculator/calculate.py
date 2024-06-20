f16b83e8cd96bd5540bc7b6ee2aaa1e2a008ee25
David Nanjila <nanjiladavid2@gmail.com>
2024-06-20 22:47:35 +0300

    Feat: Create a function to perform multiplication of a number

--- calculator/calculate.py
+++ calculator/calculate.py
@@ -3,10 +3,12 @@ def add(number1, number2):
     return  number1 + number2
 def subtract(number1, number2):
     return(number1, number2)
+def multiplication(number1, number2):
+    return number1 * number2
 
 def main():
     while True:
-        print("\nSelect: \n1. For addition\n2.For subtraction\n0.To exit")
+        print("\nSelect: \n1. For addition\n2.For subtraction\n3. For multiplication\n0.To exit")
         choice = int(input("Choice: "))
         if choice == 1:
             num1 = int(input("Enter the first number: "))
@@ -15,9 +17,13 @@ def main():
         elif choice == 2:
             num1 = int(input("Enter the first number: "))
             num2 = int(input('Enter the second number: '))
-            subtract(num1,num2)
-        elif choice == -1:
-            exit
+            print(f"The difference of {num1} and {num2} = {subtract(num1, num2)}")
+        elif choice == 3:
+            num1 = int(input("Enter the first number: "))
+            num2 = int(input('Enter the second number: '))
+            print(f"\n**The product of {num1} and {num2} = {multiplication(num1, num2)}")
+        elif choice == 0:
+            break
     
 if __name__ == '__main__':
     main()
