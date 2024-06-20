7a90e2f6b3773fe5c7a2f2719a2a41d726122337
David Nanjila <nanjiladavid2@gmail.com>
2024-06-20 20:39:47 +0300

    Feat: add a functio to allow a user to delete an item from the todo list

--- todolist/todo_list.py
+++ todolist/todo_list.py
@@ -32,13 +32,28 @@ def marks_task_done():
     list_todos(list_of_todo)
     task_number = int(input("Enter teh task number you want to marks as one: ")) - 1
     if task_number > 0 and task_number < len(list_of_todo):
-        list_of_todo[task_number]['Done'] = True
+        done_task = list_of_todo[task_number]['Done'] = True
         save_todo_task(list_of_todo)
-        print(f"Task Marked as done")
+        print(f"Task {done_task['Task']}Marked as done")
+    else:
+        print('Invalid Task number!')
+    
+def delete_task():
+    list_of_todo = upload_todo_list()
+    list_todos(list_of_todo)
+    task_number = int(input('Enter the task number you want to delete: ')) -1
+
+    if task_number > 0 and task_number < len(list_of_todo):
+        deleted_task = list_of_todo.pop(task_number)
+        save_todo_task(list_of_todo)
+        print(f'Taks succefully deleted!')
+    else:
+        print('Invalid task number try check and try again!')
 
 def main():
     while True:
-        print("Select \n1.To view tasks in the list: \n2.To add task to the list: \n3.To mark a task as done\n0.To exit: ")
+        print("Select \n1.To view tasks in the list: \n2.To add task to the list: ")
+        print("3.To mark a task as done\n4.To delete from the list \n0.To exit: ")
         choice = int(input("Choice: "))
 
         if choice == 1:
@@ -48,6 +63,8 @@ def main():
             add_todo_task()
         elif choice == 3:
             marks_task_done()
+        elif choice == 4:
+            delete_task()
         elif choice == 0:
             break
 
