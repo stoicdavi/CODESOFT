7137ec5878d7a6c4deedd1fe9990a8480bf475ed
David Nanjila <nanjiladavid2@gmail.com>
2024-06-20 18:08:07 +0300

    Feat: add a function to allow a user a mark a task as done

--- todolist/todo_list.py
+++ todolist/todo_list.py
@@ -27,10 +27,18 @@ def add_todo_task():
 def save_todo_task(list_of_todo):
     with open(TODO_LIST_FILE, 'w') as wf:
         json.dump(list_of_todo, wf, indent=3)
+def marks_task_done():
+    list_of_todo = upload_todo_list()
+    list_todos(list_of_todo)
+    task_number = int(input("Enter teh task number you want to marks as one: ")) - 1
+    if task_number > 0 and task_number < len(list_of_todo):
+        list_of_todo[task_number]['Done'] = True
+        save_todo_task(list_of_todo)
+        print(f"Task Marked as done")
 
 def main():
     while True:
-        print("Select \n1.To view tasks in the list: \n2.To add task to the list:\n0.To exit: ")
+        print("Select \n1.To view tasks in the list: \n2.To add task to the list: \n3.To mark a task as done\n0.To exit: ")
         choice = int(input("Choice: "))
 
         if choice == 1:
@@ -38,6 +46,8 @@ def main():
             list_todos(list_of_todo)
         elif choice == 2:
             add_todo_task()
+        elif choice == 3:
+            marks_task_done()
         elif choice == 0:
             break
 
