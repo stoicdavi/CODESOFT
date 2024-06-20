91c965c2e4450b5efd8c3be13ca337189ee1c68a
David Nanjila <nanjiladavid2@gmail.com>
2024-06-20 22:33:12 +0300

    Feat: Define the main function

---
+++ calculator/REDAME.md
@@ -0,0 +1,20 @@
+def add(number1, number2):
+    return  number1 + number2
+
+def subtract(number1, number2):
+    return(number1, number2)
+
+def divide(number1, number2):
+    return number1 / number2
+
+def modulus(number1, number2):
+    return number1 % number2
+
+def squareroot(number):
+    return mt.sqrt(number)
+
+def square(number):
+    return number ** number
+
+def multiplication(number1, number2):
+    return number1 * number2
--- calculator/calculate.py
+++ calculator/calculate.py
@@ -3,3 +3,17 @@ def add(number1, number2):
     return  number1 + number2
 def subtract(number1, number2):
     return(number1, number2)
+
+def main():
+    while True:
+        print("\nSelect: \n1. For addition\n2.For subtraction\n0.To exit")
+        choice = int(input("Choice: "))
+        if choice == 1:
+            num1 = int(input("Enter the first number: "))
+            num2 = int(input('Enter the second number: '))
+            print(f"The sum of {num1} and {num2} = {add(num1, num2)}")
+        elif choice == 2:
+            num1 = int(input("Enter the first number: "))
+            num2 = int(input('Enter the second number: '))
+            subtract(num1,num2)
+    
