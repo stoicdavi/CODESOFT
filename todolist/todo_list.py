1fb3ac97b163bf90ed3f0dbd215a97cca98e6901
David Nanjila <nanjiladavid2@gmail.com>
2024-06-20 14:43:58 +0300

    Feat: Create a todo.json file to store data and a function to read the data from the json file, a print funtion to chech wheter the data is loaded

--- todolist/todo_list.py
+++ todolist/todo_list.py
@@ -1,4 +1,14 @@
 import os
 import json
 
-TODO_LIST = 'todo.json'
+TODO_LIST_FILE = 'todo.json'
+
+#loading our json data
+def upload_todo_list():
+    if os.path.exists(TODO_LIST_FILE):
+        with open(TODO_LIST_FILE, 'r') as rf:
+            return json.load(rf)
+    else:
+        return []
+    
+print(upload_todo_list())
