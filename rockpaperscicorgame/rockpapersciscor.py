0db22045a0cc6ffec3c31095470416fe0c010d0f
David Nanjila <nanjiladavid2@gmail.com>
2024-06-22 13:54:33 +0300

    feat: Create a function to determine the winner based on the user and the computers selection

--- rockpaperscicorgame/rockpapersciscor.py
+++ rockpaperscicorgame/rockpapersciscor.py
@@ -5,7 +5,16 @@ def get_players_choice():
     while player_choice not in options:
         player_choice = input('Invalid choice! Please enter (paper, rock or scissor): ')
     return player_choice
+computer_choice = rd.choice(options)
+def determine_winner(player_choice, computer_choice):
+    if  (player_choice == "rock" and computer_choice == "scissors") or \
+         (player_choice== "scissors" and computer_choice == "paper") or \
+         (player_choice == "paper" and computer_choice == "rock"):
+        return "Player"
+    elif player_choice == computer_choice:
+        return 'Tie'
+    else:
+        return 'Computer'
 
-computer = rd.choice(options)
-print(f'Player choice: {get_players_choice()}')
-print(f'Computers choice: {computer}')
+player_choice = get_players_choice()
+print(f'The winner is : {determine_winner(player_choice, computer_choice)}')
