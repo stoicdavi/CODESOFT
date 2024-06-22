593ce8e0c6544d2642793a5b94b8a0d26e1c75b0
David Nanjila <nanjiladavid2@gmail.com>
2024-06-22 13:40:58 +0300

    feat: Create a functio to get the users choice

--- rockpaperscicorgame/rockpapersciscor.py
+++ rockpaperscicorgame/rockpapersciscor.py
@@ -1,9 +1,11 @@
 import random as rd
+options = ('paper', 'rock', 'scissor')
+def get_players_choice():
+    player_choice = input("Choose an options (paper, rock or scissor): ").lower()
+    while player_choice not in options:
+        player_choice = input('Invalid choice! Please enter (paper, rock or scissor): ')
+    return player_choice
 
-options = ('paper', 'rock', 'sciscor')
-
-player = input("Choose an options (paper, rock or sciscor): ")
 computer = rd.choice(options)
-
-print(f'Player choice: {player}')
+print(f'Player choice: {get_players_choice()}')
 print(f'Computers choice: {computer}')
